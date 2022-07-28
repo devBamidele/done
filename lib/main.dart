2a3804ec0dab262d8d567c9fdc9c0569d8f7c28e
Bamidele Ajewole <bamideledavid.femi@gmail.com>
2022-07-28 22:11:38 +0100

    End of Day5: Added CRUD methods but I have not used them yet

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,89 @@ import 'package:responsive_framework/responsive_framework.dart';
 import 'package:sqflite/sqflite.dart';
 
 import 'data/data.dart';
+import 'models/data_class.dart';
 
 main() async {
   runApp(const Done());
   WidgetsFlutterBinding.ensureInitialized();
   final database = openDatabase(
     join(await getDatabasesPath(), 'done_database.db'),
+    onCreate: (db, version) {
+      // Run the CREATE TABLE statement on the database.
+      return db.execute(
+        'CREATE TABLE done(id INTEGER PRIMARY KEY, task TEXT, complete INTEGER, category TEXT)',
+      );
+    },
+    // Set the version. This executes the onCreate function and provides a
+    // path to perform database upgrades and downgrades.
+    version: 1,
   );
+
+  // Define a function that inserts tasks into the database
+  Future<void> insertTask(DataClass item) async {
+    // Get a reference to the database.
+    final db = await database;
+
+    // Insert the task into the correct table. You might also specify the
+    // `conflictAlgorithm` to use in case the same task is inserted twice.
+    //
+    // In this case, replace any previous data.
+    await db.insert(
+      'done',
+      item.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  // A method that retrieves all the tasks from the done table
+  Future<List<DataClass>> viewTasks() async {
+    // Get a reference to the database.
+    final db = await database;
+
+    // Query the table for all The Dogs.
+    final List<Map<String, dynamic>> maps = await db.query('done');
+
+    // Convert the List<Map<String, dynamic> into a List<Dog>.
+    return List.generate(maps.length, (i) {
+      return DataClass(
+        id: maps[i]['id'],
+        task: maps[i]['task'],
+        complete: maps[i]['complete'],
+        category: maps[i]['category'],
+      );
+    });
+  }
+
+  // The method that updates tasks
+  Future<void> updateTask(DataClass item) async {
+    // Get a reference to the database.
+    final db = await database;
+
+    // Update the given Dog.
+    await db.update(
+      'done',
+      item.toMap(),
+      // Ensure that the Dog has a matching id.
+      where: 'id = ?',
+      // Pass the Dog's id as a whereArg to prevent SQL injection.
+      whereArgs: [item.id],
+    );
+  }
+
+  // Clear a task from the table using a specific id
+  Future<void> deleteTask(int id) async {
+    // Get a reference to the database.
+    final db = await database;
+
+    // Remove the Dog from the database.
+    await db.delete(
+      'done',
+      // Use a `where` clause to delete a specific dog.
+      where: 'id = ?',
+      // Pass the Dog's id as a whereArg to prevent SQL injection.
+      whereArgs: [id],
+    );
+  }
 }
 
 class Done extends StatelessWidget {
--- lib/models/data_class.dart
+++ lib/models/data_class.dart
@@ -1,13 +1,31 @@
 class DataClass {
   int id;
   String task;
+  int complete;
   String category;
-  bool complete;
 
   DataClass({
     required this.id,
     required this.task,
-    required this.category,
     required this.complete,
+    required this.category,
   });
+
+  // Convert a [the DataClass] into a Map. The keys must correspond to the names of the
+  // columns in the database.
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'task': task,
+      'complete': complete,
+      'category': category,
+    };
+  }
+
+  // Implement toString to make it easier to see information about
+  // each item when using the print statement.
+  @override
+  String toString() {
+    return 'Task{id: $id, task: $task, complete: $complete, category $category}';
+  }
 }
